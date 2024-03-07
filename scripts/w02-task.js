6c50cbb8c17c862e93efbfdde1dc18b616197425
craig201042 <walila12@yahoo.com.tw>
2024-03-07 20:16:01 +0800

    correct modification

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,24 +4,24 @@
 
 /* Step 2 - Variables */
 
-const fullName= 'Craig Zeng';
+const fullName='Craig Zeng';
 const currentYear='2024';
-const profilePicture='images/craig.png';
+const profilePicture='images/craig.jpg';
 
 
 /* Step 3 - Element Variables */
 
 const nameElement=document.getElementById('name');
 const foodElement=document.getElementById('food');
-const yearElement=document.querrySelector('#year');
-const imageElement=document.querrySelector('img');
+const yearElement=document.querySelector('#year');
+const imageElement=document.querySelector('img');
 
 
 /* Step 4 - Adding Content */
 
-nameElement.innerHTML='<strong>${fullName}</strong>';
+nameElement.innerHTML=`<strong>${fullName}</strong>`;
 yearElement.textContent =currentYear;
-imageElement.setAttribute('src', 'images/craig.png');
+imageElement.setAttribute('src', profilePicture);
 imageElement.setAttribute('alt', 'Profile image of ${fullName}');
 
 
@@ -31,11 +31,11 @@ const foods=['Hamburger', 'beef noodle', 'milkshake'];
 foodElement.innerHTML=foods;
 let singleFood='pizza';
 foods.push(singleFood);
-foodElement.innerHTML+='<br>${foods}';
+foodElement.innerHTML+= `<br>${foods}`;
 foods.shift();
-foodElement.innerHTML+='<br>${foods}';
+foodElement.innerHTML+= `<br>${foods}`;
 foods.pop();
-foodElement.innerHTML+='<br>${foods}';
+foodElement.innerHTML+= `<br>${foods}`;
 
 
 
